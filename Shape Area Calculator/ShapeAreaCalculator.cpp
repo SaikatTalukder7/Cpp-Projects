64a57793a71f7ffa42ae117cc9a2959fd1062176
Saikat Talukder <saikat60mu@gmail.com>
2025-08-30 13:57:53 +0600

    Add files via upload

---
+++ Shape Area Calculator/ShapeAreaCalculator.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Circle()
+{
+    double r;
+    cout << "Enter the radius of the Circle: ";
+    cin >> r;
+    double area = M_PI * r * r;
+    cout << "Circle Area: " << area << endl;
+}
+void Square()
+{
+    double a;
+    cout << "Enter the side of the Square: ";
+    cin >> a;
+    double area = a * a;
+    cout << "Square Area: " << area << endl;
+}
+void Rectangle()
+{
+    double l, w;
+    cout << "Enter the length of the Rectangle: ";
+    cin >> l;
+    cout << "Enter the width of the Rectangle: ";
+    cin >> w;
+    double area = w * l;
+    cout << "Rectangle Area: " << area << endl;
+}
+
+int main()
+{
+    int choice;
+
+    cout << "Select one form these:\n";
+    cout << "1. Circle\n";
+    cout << "2. Square\n";
+    cout << "3. Rectangle\n";
+
+    cout << "Enter choice:";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        Circle();
+        break;
+    case 2:
+        Square();
+        break;
+    case 3:
+        Rectangle();
+        break;
+    default:
+        cout << "Invalid choice!";
+    }
+    return 0;
+}
