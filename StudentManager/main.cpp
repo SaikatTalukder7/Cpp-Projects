92e50cab032a2417b8f8dca4d0ab2aad2346d0db
Saikat Talukder <saikat60mu@gmail.com>
2025-09-11 18:52:54 +0600

    Create main.cpp

---
+++ StudentManager/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StudentInformation{
+    int ID;
+    string Name;
+    int age;
+    string Department;
+    float CGPA;
+};
+void addStudent()
+{
+    StudentInformation s;
+}
+
+int main()
+{
+    
+}
